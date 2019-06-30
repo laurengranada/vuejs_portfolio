1d61441cbad0195dd79c1a5a7f1e5100c3cc70ed
laurengranada <laurengranada@utexas.edu>
2019-06-30 16:01:25 -0500

    adding for deployment

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,14 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'www.laurengranada.com' > CNAME
+# echo 'www.laurengranada.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:laurengranada/laurengranada.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
